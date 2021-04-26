98543fe87a0bbd2dfbd24b1049392fe1725d111b
EJ TSENG <fogjogger1992@gmail.com>
2021-04-26 10:30:50 +0800

    feat: add timestamp

--- app.js
+++ app.js
@@ -3,6 +3,25 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+app.use(function (req, res, next) {
+  const startTime = Date.now()
+  const time = new Date().toLocaleString().split(" ")[1]
+  const monthDateYear = new Date().toLocaleString().split(",")[0]
+  const month = monthDateYear.split("/")[0]
+  const date = monthDateYear.split("/")[1]
+  const year = monthDateYear.split("/")[2]
+
+  res.on('finish', function () {
+    const finishTime = Date.now()
+    const duration = finishTime - startTime
+    console.log(`${year}-${month}-${date} ${time} | ${req.method} from ${req.originalUrl} | total time: ${duration} ms`)
+  })
+
+  next()
+})
+
+
 app.get('/', (req, res) => {
   res.send('列出全部 Todo')
 })
